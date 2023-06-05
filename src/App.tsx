514dfdf7c76ed6f601dcaf8a454aa9152a73da5a
unknown <arsen.lulukyan@gmail.com>
2023-06-05 22:13:01 +0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import './App.scss';
+import { Header } from './components/Layout';
+
+const MainContent = () => {
+  return (
+    <Container>
+      <h1>Заголовок страницы</h1>
+      <p>Основное содержимое страницы...</p>
+    </Container>
+  );
+};
 
 const App = () => {
   return (
-    <>
-    </>
-  )
-}
+    <div>
+      <Header />
+      <MainContent />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,25 @@
+import { Container, Navbar } from 'react-bootstrap';
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className="bg-white">
+      <Navbar>
+        <Container>
+          <Navbar.Brand className={styles.brand}>INVENTORY</Navbar.Brand>
+          <div>
+            <div>
+              <p>Вторник</p>
+            </div>
+            <div className="d-flex mt-2">
+              <p>06 Апр, 2017</p>
+              <p className="ml-2">17:20</p>
+            </div>
+          </div>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
