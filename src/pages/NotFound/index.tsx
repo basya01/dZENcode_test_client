bfe171117d2491aebb386d14e9bb0186231642ab
unknown <arsen.lulukyan@gmail.com>
2023-06-17 12:59:20 +0300

    add not found page

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './NotFound.module.scss';
+
+const NotFound = () => {
+  return (
+    <div className={styles.notFound}>
+      <p className={styles.notFound__cat}>😿</p>
+      <div className="text-center">
+        <h1 className="h1">404</h1>
+        <p className="h4">Страница не найдена</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
 export { default as Orders } from './Orders';
 export { default as Products } from './Products';
+export { default as NotFound } from './NotFound';
