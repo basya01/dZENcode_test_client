fe4712df8d178587e5d5f678bbb06e0b29b86bb1
unknown <arsen.lulukyan@gmail.com>
2023-06-17 09:39:40 +0300

    add animation

--- src/components/Layout/Main/index.tsx
+++ src/components/Layout/Main/index.tsx
@@ -1,5 +1,8 @@
+import clsx from 'clsx';
 import { Col } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
+
+import styles from './Main.module.scss';
 
 const Main = () => {
   return (
@@ -7,7 +10,7 @@ const Main = () => {
       as="main"
       sm={9}
       md={10}
-      className="mt-5 px-5"
+      className={clsx('mt-5 px-5', styles.animated_routes_container)}
     >
       <Outlet />
     </Col>
